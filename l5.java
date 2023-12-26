874679812c765f8d4452bc3fe913b8867ec18e99
Aarya Paradkar <aaru7276@gmail.com>
2023-12-26 19:58:58 +0530

    added inverted half pyramid

--- l5.java
+++ l5.java
@@ -32,15 +32,28 @@ public class l5 {
         // }
 
         // half pyramid
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter the number of rows: ");
+        // int n = sc.nextInt();
+
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        // inverted half pyramid
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of rows: ");
         int n = sc.nextInt();
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = n; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
+
     }
 }
