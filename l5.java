d7ef40c879a17de0a71fb1dbc0a5fa328449a3e5
Aarya Paradkar <aaru7276@gmail.com>
2023-12-27 15:22:09 +0530

    added floyd's triangle and 0-1 triangle

--- l5.java
+++ l5.java
@@ -86,13 +86,44 @@ public class l5 {
         // }
 
         // inverted half pyramid with numbers
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter the number of columns: ");
+        // int n = sc.nextInt();
+
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= n - i + 1; j++) {
+        // System.out.print(j + " ");
+        // }
+        // System.out.println();
+        // }
+
+        // Floyd's triangle
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter the number of rows: ");
+        // int n = sc.nextInt();
+        // int number = 1;
+
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(number + " ");
+        // number++;
+        // }
+        // System.out.println();
+        // }
+
+        // 01 triangle
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of columns: ");
         int n = sc.nextInt();
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n - i + 1; j++) {
-                System.out.print(j + " ");
+            for (int j = 1; j <= i; j++) {
+                int sum = i + j;
+                if (sum % 2 == 0) {
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
             }
             System.out.println();
         }
