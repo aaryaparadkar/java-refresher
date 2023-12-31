3e1dec600f751837eefe19cd888e937107d652b0
Aarya Paradkar <aaru7276@gmail.com>
2024-01-01 00:37:48 +0530

    added solid rhombus

--- l6.java
+++ l6.java
@@ -10,30 +10,43 @@ public class l6 {
 
         // Butterfly pattern
         // upper half
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
-            }
-            int spaces = 2 * (n - i);
-            for (int j = 1; j <= spaces; j++) {
-                System.out.print("  ");
-            }
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("* ");
+        // }
+        // int spaces = 2 * (n - i);
+        // for (int j = 1; j <= spaces; j++) {
+        // System.out.print(" ");
+        // }
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
 
-        // lower half
-        for (int i = n; i >= 1; i--) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
+        // // lower half
+        // for (int i = n; i >= 1; i--) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("* ");
+        // }
+        // int spaces = 2 * (n - i);
+        // for (int j = 1; j <= spaces; j++) {
+        // System.out.print(" ");
+        // }
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
+
+        // solid rhombus
+        for (int i = 1; i <= n; i++) {
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
             }
-            int spaces = 2 * (n - i);
-            for (int j = 1; j <= spaces; j++) {
-                System.out.print("  ");
-            }
-            for (int j = 1; j <= i; j++) {
+            // stars
+            for (int j = 1; j <= n; j++) {
                 System.out.print("* ");
             }
             System.out.println();
