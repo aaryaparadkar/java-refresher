61aac0f3aa103abbc273607e48b8a4410732a971
Aarya Paradkar <aaru7276@gmail.com>
2024-01-02 13:59:57 +0530

    added number pyramid

--- l6.java
+++ l6.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 public class l6 {
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number of rows: ");
-        int n = sc.nextInt();
-        System.out.print("Enter the number of columns: ");
-        int m = sc.nextInt();
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter the number of rows: ");
+        // int n = sc.nextInt();
+        // System.out.print("Enter the number of columns: ");
+        // int m = sc.nextInt();
 
         // Butterfly pattern
         // upper half
@@ -40,14 +40,27 @@ public class l6 {
         // }
 
         // solid rhombus
+        // for (int i = 1; i <= n; i++) {
+        // // spaces
+        // for (int j = 1; j <= n - i; j++) {
+        // System.out.print(" ");
+        // }
+        // // stars
+        // for (int j = 1; j <= n; j++) {
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
+
+        // number pyramid
+        int n = 5;
         for (int i = 1; i <= n; i++) {
             // spaces
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-            // stars
-            for (int j = 1; j <= n; j++) {
-                System.out.print("* ");
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
             }
             System.out.println();
         }
