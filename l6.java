5ab9f19a6b817b88a1840433db1c62312ab97f35
Aarya Paradkar <aaru7276@gmail.com>
2024-01-02 14:37:30 +0530

    added diamond pattern

--- l6.java
+++ l6.java
@@ -66,21 +66,51 @@ public class l6 {
         // }
 
         // palindromic pattern
-        int n = 5;
+        // int n = 5;
+        // for (int i = 1; i <= n; i++) {
+        // // spaces
+        // for (int j = 1; j <= n - i; j++) {
+        // System.out.print(" ");
+        // }
+
+        // // first half
+        // for (int j = i; j >= 1; j--) {
+        // System.out.print(j);
+        // }
+
+        // // second half
+        // for (int j = 2; j <= i; j++) {
+        // System.out.print(j);
+        // }
+        // System.out.println();
+        // }
+
+        // diamond pattern
+        int n = 4;
+        // upper half
         for (int i = 1; i <= n; i++) {
             // spaces
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
 
-            // first half
-            for (int j = i; j >= 1; j--) {
-                System.out.print(j);
+            // stars
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // lower half
+        for (int i = n; i >= 1; i--) {
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
             }
 
-            // second half
-            for (int j = 2; j <= i; j++) {
-                System.out.print(j);
+            // stars
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
