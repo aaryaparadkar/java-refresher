bcbd10c1b4f880767c003b0bc51533f205e299a7
Aarya Paradkar <aaru7276@gmail.com>
2024-01-02 14:26:36 +0530

    added palindromic pattern

--- l6.java
+++ l6.java
@@ -53,14 +53,34 @@ public class l6 {
         // }
 
         // number pyramid
+        // int n = 5;
+        // for (int i = 1; i <= n; i++) {
+        // // spaces
+        // for (int j = 1; j <= n - i; j++) {
+        // System.out.print(" ");
+        // }
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(i + " ");
+        // }
+        // System.out.println();
+        // }
+
+        // palindromic pattern
         int n = 5;
         for (int i = 1; i <= n; i++) {
             // spaces
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= i; j++) {
-                System.out.print(i + " ");
+
+            // first half
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+
+            // second half
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
             }
             System.out.println();
         }
